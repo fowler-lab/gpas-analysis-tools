37aa4356022bb8b514ae16be78982a04bb5453c2
Philip Fowler <philipwfowler@gmail.com>
2026-04-09 09:37:23 +0000

    updated to work with NTMs..

---
+++ src/gpas_analysis_tools/genetics.py
@@ -0,0 +1,249 @@
+import pandas, numpy, glob, json
+
+import pyarrow.parquet as pq
+
+from tqdm import tqdm
+
+tqdm.pandas()
+
+def parse_variants(row):
+
+    variant = row.variant
+    is_null = False
+    is_minor = False
+    minor_variant = None
+    minor_reads = 0
+    coverage = 0
+
+    a = json.loads(row["vcf_evidence"])
+
+    # From Jeremy:
+    #  "Just heard back from Martin, because of minos mapping
+    #   against a graph rather than genome, COV_TOTAL is reads
+    #   mapping to the site (so reads can map to multiple sites
+    #   in an allele), whereas COV is unique reads mapping to
+    #   the allele, which is why COV_TOTAL can be bigger. So
+    #   I think sum(COV) is what we should be using here"
+
+    # if "COV_TOTAL" in a.keys():
+    #     coverage = a["COV_TOTAL"]
+    if "COV" in a.keys():
+        if isinstance(a["COV"], list):
+            coverage = sum(a["COV"])
+        elif isinstance(a["COV"], int):
+            coverage = a["COV"]
+        else:
+            coverage = -1
+
+    # FRS can be the wrong way round so ignore
+    # if "FRS" in a.keys():
+    #     frs = a["FRS"]
+    #     if frs == ".":
+    #         frs = None
+
+    if variant[-1] == "x":
+        is_null = True
+    elif ":" in variant:
+        is_minor = True
+        cols = variant.split(":")
+        minor_variant = cols[0]
+        minor_reads = int(cols[1])
+        if "ins" in variant or "del" in variant:
+            variant = minor_variant.split("_")[0] + "_minorindel"
+        else:
+            variant = minor_variant[:-1] + "z"
+
+    return pandas.Series(
+        [variant, is_null, is_minor, minor_variant, minor_reads, coverage]
+    )
+
+
+def parse_mutations(row):
+
+    mutation = row.mutation
+    is_null = False
+    is_minor = False
+    minor_mutation = None
+    minor_reads = None
+
+    if ":" in mutation:
+        is_minor = True
+        cols = mutation.split(":")
+        minor_mutation = cols[0]
+        minor_reads = int(cols[1])
+        if "ins" in mutation or "del" in mutation:
+            mutation = mutation.split("_")[0] + "_minorindel"
+        else:
+            if minor_mutation[0] in ["a", "t", "c", "g"]:
+                mutation = minor_mutation[:-1] + "z"
+            else:
+                mutation = minor_mutation[:-1] + "Z"
+    elif mutation[-1] in ["X", "x"]:
+        is_null = True
+
+    return pandas.Series([mutation, is_null, is_minor, minor_mutation, minor_reads])
+
+
+def build_genetics_table(filename, data_path, tables_path, master_table, max_samples, chunks, uppercase = True):
+
+    tables = []
+    n_samples = 0
+
+    species_table = pandas.read_parquet(tables_path / "SPECIES.parquet")
+
+    n_files = sum(1 for i in (data_path).rglob("*" + filename + "*.csv"))
+
+    for i in tqdm((data_path).rglob("*" + filename + "*.csv"), total=n_files):
+
+        df = pandas.read_csv(i)
+
+        n_samples += 1
+        if max_samples is not None and n_samples > max_samples:
+            break
+
+        uid = i.stem.split("_")[0]
+        if filename in uid:
+            uid = uid.split('.'+filename)[0]
+
+        # let's check the uid is at least in the master_table!
+        assert uid in master_table.index, f"UID {uid} not found in master table"
+        
+        # check to see if the same has the 'Assembled NTM Results' block in the main report
+        if master_table.at[uid, 'has_new_block_in_main_report']:
+            for sn in list(species_table.at[uid, 'SPECIES_NAME']):
+                if sn.replace(' ','_') in i.stem:
+                    species_name = sn
+        else:
+            species_name = species_table.at[uid, 'SPECIES_NAME']
+        df.insert(1, 'species_name', species_name)
+
+        species_table.at[uid, "has_" + filename] = True
+        tables.append(df)
+
+    df = pandas.concat(tables)
+
+    if filename == "effects":
+        for col in [
+            "species_name",
+            "gene",
+            "drug",
+            "prediction",
+            "catalogue_name",
+            "prediction_values",
+        ]:
+            df[col] = df[col].astype("category")
+
+        df = df.rename(columns=str.upper)
+        df.set_index(
+            [
+                "UNIQUEID",
+                "SPECIES_NAME",
+                "CATALOGUE_NAME",
+                "CATALOGUE_VERSION",
+                "PREDICTION_VALUES",
+                "DRUG",
+                "GENE",
+                "MUTATION",
+            ],
+            inplace=True,
+        )
+
+    elif filename == "predictions":
+        for col in [
+            "species_name",
+            "drug",
+            "prediction",
+            "catalogue_name",
+            "catalogue_values",
+        ]:
+            df[col] = df[col].astype("category")
+
+        df = df.rename(columns=str.upper)
+        df.set_index(
+            [
+                "UNIQUEID",
+                "SPECIES_NAME",
+                "CATALOGUE_NAME",
+                "CATALOGUE_VERSION",
+                "CATALOGUE_VALUES",
+                "DRUG",
+            ],
+            inplace=True,
+        )
+
+    elif filename == "variants":
+        tables = []
+        counter = 0
+        for df_i in tqdm(numpy.array_split(df, chunks)):
+            df_i[
+                [
+                    "var",
+                    "is_null",
+                    "is_minor",
+                    "minor_variant",
+                    "minor_reads",
+                    "coverage",
+                ]
+            ] = df_i.apply(parse_variants, axis=1)
+            df_i.drop(columns=["variant"], inplace=True)
+            df_i.rename(columns={"var": "variant"}, inplace=True)
+            for col in [
+                "gene",
+                "species_name",
+            ]:
+                df_i[col] = df_i[col].astype("category")
+
+            df_i = df_i.rename(columns=str.upper)
+            df_i.set_index(["UNIQUEID", "SPECIES_NAME", "GENE", "VARIANT"], inplace=True)
+            df_i.to_csv(
+                str(tables_path / (filename.upper() + "_" + str(counter)))
+                + ".csv"
+            )
+            df_i.drop(columns=["VCF_EVIDENCE"], inplace=True)
+            df_i.to_parquet(
+                str(tables_path / (filename.upper() + "_" + str(counter)))
+                + ".parquet"
+            )
+
+            tables.append(df_i)
+            counter += 1
+        df = pandas.concat(tables)
+
+        files = glob.glob(str(tables_path) + "/VARIANTS_*.parquet")
+        schema = pq.ParquetFile(files[0]).schema_arrow
+        with pq.ParquetWriter(
+            str(tables_path) + "/VARIANTS.parquet", schema=schema
+        ) as writer:
+            for file in tqdm(files):
+                writer.write_table(pq.read_table(file, schema=schema))
+
+    elif filename == "mutations":
+        tables = []
+        for df_i in tqdm(numpy.array_split(df, chunks)):
+            df_i[
+                ["mut", "is_null", "is_minor", "minor_mutation", "minor_reads"]
+            ] = df_i.apply(parse_mutations, axis=1)
+            df_i.drop(columns=["mutation"], inplace=True)
+            df_i.rename(columns={"mut": "mutation"}, inplace=True)
+            for col in [
+                "species_name",
+                "gene",
+                "ref",
+                "alt",
+                "amino_acid_sequence",
+                "minor_mutation",
+            ]:
+                df_i[col] = df_i[col].astype("category")
+
+            df_i = df_i.rename(columns=str.upper)
+            df_i.set_index(["UNIQUEID", "SPECIES_NAME", "GENE", "MUTATION"], inplace=True)
+
+            tables.append(df_i)
+
+        df = pandas.concat(tables)
+
+    if filename != "variants":
+        df.to_csv(str(tables_path / filename.upper()) + ".csv", index=True)
+        df.to_parquet(str(tables_path / filename.upper()) + ".parquet")
+
+    return species_table
---
+++ src/gpas_analysis_tools/species.py
@@ -0,0 +1,195 @@
+import pandas
+import json
+
+from tqdm import tqdm
+
+tqdm.pandas()
+
+def tidy_uid(row):
+    if 'main' in row.RUN_ACCESSION:
+        return row.RUN_ACCESSION.split('.main')[0]
+    else:
+        return row.RUN_ACCESSION
+    
+def tidy_species(row):
+    if 'tuberculosis' in row.MAPPED_SPECIES:
+        species = 'Mycobacterium tuberculosis'
+        subspecies = row.MAPPED_SPECIES.split(' (')[0].replace('M. ','Mycobacterium ')   
+        lineage = row.LINEAGE
+        sublineage = row.SUBLINEAGE
+    elif isinstance(row.LINEAGE, str) and 'subsp.' in row.LINEAGE:
+        species = row.MAPPED_SPECIES # + ' complex'
+        subspecies = row.LINEAGE
+        lineage = None
+        sublineage = None
+    else:
+        species = row.MAPPED_SPECIES
+        subspecies = None
+        lineage = row.LINEAGE
+        sublineage = row.SUBLINEAGE
+    return pandas.Series({'SPECIES': species, 'SUBSPECIES': subspecies, 'LINEAGE': lineage, 'SUBLINEAGE': sublineage})  
+
+
+def split_species(row):
+    cols = row["name"].split(" (")
+    species = cols[0]
+    return species
+
+def get_species_number(data):
+    # let's get a list of the mapped species names
+    #  we assume here that if it doesn't have a new block there will be only one species!
+    has_new_block = False
+    if 'Assembled NTM Results' in data:
+        species_number = len(data['Assembled NTM Results']['Assembled Species'])
+        has_new_block = True
+    else:
+        assert len(data['Mycobacterium Results']['Species']) == 1, f"Expected only one species for {uid} but found {len(data['Mycobacterium Results']['Species'])}"
+        species_number = 1
+
+    return (species_number, has_new_block)
+
+def parse_lineage(lineage_results):
+        
+    if lineage_results["Name"][:7] == "lineage":
+        if lineage_results["Name"][7] in ["B","C",]:
+            lineage = lineage_results["Name"]
+        else:
+            lineage = lineage_results["Name"][:8]
+        sublineage = lineage_results["Name"]
+    else:
+        lineage = lineage_results["Name"].replace("_", " ")
+        sublineage = ""
+    
+    return lineage, sublineage
+    
+
+
+def build_species_table(data_path = None, 
+    tables_path = None,
+    master_table = None, 
+    max_samples = None, 
+):
+    successful_genome = 0
+    too_few_reads_genome = 0
+    too_few_reads_id = 0
+
+    tables = []
+    n_samples = 0
+
+    # discover all the main_report.json files and loop through them
+    for i in tqdm((data_path).rglob("*main_report*.json")):
+
+        uid = i.stem.split("_")[0]
+        if 'main' in uid:
+            uid = uid.split('.main')[0]
+
+        # let's check the uid is at least in the master_table!
+        assert uid in master_table.index, f"UID {uid} not found in master table"
+
+        # record that it has a main_report.json file
+        master_table.at[uid, "has_main_report"] = True
+
+        f = open(i)
+        data = json.load(f)
+        n_samples += 1
+        if max_samples is not None and n_samples > max_samples:
+            print('stopping')
+            break
+
+        if (
+            "Mycobacterial species identified" in data["Pipeline Outcome"]
+        ):
+            too_few_reads_genome += 1
+            master_table.at[uid, "status"] = "cannot assemble"
+            # continue
+        elif (
+            "Number of Mycobacterial reads is too low" in data["Pipeline Outcome"]
+        ):
+            too_few_reads_id += 1
+            master_table.at[uid, "status"] = "cannot speciate"
+            continue
+        elif (
+            "Sufficient reads mapped" in data["Pipeline Outcome"]
+        ):
+            master_table.at[uid, "status"] = "complete"
+            successful_genome += 1
+        else:
+            print(uid, data["Pipeline Outcome"])
+            continue
+
+        master_table.at[uid, "HUMAN_READS"] = data['Organism Identification']['Human Reads']
+        master_table.at[uid, "MYCOBACTERIAL_READS"] = data['Organism Identification']['Mycobacterium Reads']
+        master_table.at[uid, "OTHER_BACTERIAL_READS"] = data['Organism Identification']['Non-Mycobacterium Bacteria Reads']
+        master_table.at[uid, "UNCLASSIFIED_READS"] = data['Organism Identification']['Unclassified Reads']
+
+        pipeline_build = data["Metadata"]["Pipeline build"].replace("\n", "")
+        master_table.at[uid, "GPAS_PIPELINE_BUILD"] = pipeline_build
+
+        number_species, has_new_block = get_species_number(data)
+        master_table.at[uid, "has_new_block_in_main_report"] = has_new_block
+        master_table.at[uid, "number_of_species"] = number_species
+
+
+        for sn in range(number_species):
+            row = [uid]
+            if has_new_block:
+                row.append(data["Assembled NTM Results"]["Summary"][sn]["Name"])
+                row.append(data["Assembled NTM Results"]["Summary"][sn]["Num Reads"])
+                row.append(data["Assembled NTM Results"]["Summary"][sn]["Coverage"])
+                row.append(data["Assembled NTM Results"]["Summary"][sn]["Depth"])
+                if sn >= len(data["Assembled NTM Results"]["Lineage"]):
+                    lineage, sublineage = "", ""
+                else:   
+                    lineage, sublineage = parse_lineage(data["Assembled NTM Results"]["Lineage"][sn])
+            else:
+                row.append(data["Mycobacterium Results"]["Summary"][sn]["Name"])
+                row.append(data["Mycobacterium Results"]["Summary"][sn]["Num Reads"])
+                row.append(data["Mycobacterium Results"]["Summary"][sn]["Coverage"])
+                row.append(data["Mycobacterium Results"]["Summary"][sn]["Depth"])
+                lineage, sublineage = parse_lineage(data["Mycobacterium Results"]["Lineage"][sn])
+            row.append(lineage)
+            row.append(sublineage)
+            tables.append(row)
+
+    SPECIES = pandas.DataFrame(
+        tables,
+        columns=[
+            "RUN_ACCESSION",
+            "MAPPED_SPECIES",
+            "N_READS",
+            "COVERAGE",
+            "DEPTH",
+            "LINEAGE",
+            "SUBLINEAGE",
+        ],
+    )
+    SPECIES[['SPECIES_NAME', 'SUBSPECIES', 'LINEAGE', 'SUBLINEAGE']] = SPECIES.apply(tidy_species, axis=1)
+    SPECIES.drop(columns=['MAPPED_SPECIES'], inplace=True)
+    SPECIES = SPECIES[['RUN_ACCESSION', 'SPECIES_NAME', 'SUBSPECIES', 'LINEAGE', 'SUBLINEAGE','N_READS', 'COVERAGE', 'DEPTH',  ]]
+    SPECIES['RUN_ACCESSION'] = SPECIES.apply(tidy_uid, axis=1)
+
+    for col in ['SPECIES_NAME', 'SUBSPECIES', 'LINEAGE', 'SUBLINEAGE',]:
+        SPECIES[col] = SPECIES[col].astype("category")
+    for col in ['N_READS']:
+        SPECIES[col] = SPECIES[col].astype("Int64")
+    for col in ['COVERAGE', 'DEPTH']:    
+        SPECIES[col] = SPECIES[col].astype("float64")
+
+    SPECIES.set_index("RUN_ACCESSION", inplace=True)
+    SPECIES.to_csv(tables_path / "SPECIES.csv")
+    SPECIES.to_parquet(tables_path / "SPECIES.parquet")
+
+    total_genomes = successful_genome + too_few_reads_genome + too_few_reads_id
+    print(f"{total_genomes} samples were processed.")
+    print(
+        f"{successful_genome} successfully reached a genome, {too_few_reads_id} samples had too few reads for identification and {too_few_reads_genome} samples had too few reads for genome assembly"
+    )
+    
+    with pandas.option_context("future.no_silent_downcasting", True):
+        master_table = master_table.fillna(value = {
+                "has_new_block_in_main_report": False,
+                "number_of_species": 0,
+                "has_main_report": False,
+                }).infer_objects(copy=False)
+
+    return master_table
